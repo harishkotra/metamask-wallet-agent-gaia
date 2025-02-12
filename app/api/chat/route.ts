0ca5d633b5451e87b9e388bf1debca95950db975
Harish Kotra <harishkotra@users.noreply.github.com>
2025-02-13 00:17:23 +0530

    added gaia as an OpenAI replacement

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,13 +1,18 @@
-import { openai } from "@ai-sdk/openai";
+import { createOpenAI } from "@ai-sdk/openai";
 import { streamText } from "ai";
 import { tools } from "@/ai/tools";
 
 export async function POST(request: Request) {
   const { messages } = await request.json();
 
+  const openai = createOpenAI({
+    baseURL: 'https://llama8b.gaia.domains/v1',
+    apiKey: 'GAIA'
+  });
+
   try {
     const result = streamText({
-      model: openai("gpt-4o"),
+      model: openai("llama"),
       system: "you are a friendly assistant",
       messages,
       maxSteps: 5,
